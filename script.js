1e3dfcbb01b03a1d04f667d72a10be9472a66be6
dhimas <dimaseka83@gmail.com>
2022-01-25 22:14:28 +0700

    push

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
-const myAge = birthday => {
-    const birtday = new Date(birthday);
-    const today = Date.now();
+/**
+ * TODO:
+ * 1. Buatlah class bernama Animal dengan ketentuan:
+ *    - Memiliki properti:
+ *      - name: string
+ *      - age: int
+ *      - isMammal: boolean
+ *    - Memiliki constructor untuk menginisialisasi properti:
+ *      - name
+ *      - age
+ *      - isMammal
+ * 2. Buatlah class bernama Rabbit dengan ketentuan:
+ *    - Merupakan turunan dari class Animal
+ *    - Memiliki method:
+ *      - eat yang mengembalikan nilai string "${this.name} sedang makan!"
+ *    - Ketika diinstansiasi, properti isMammal harus bernilai true
+ * 3. Buatlah class bernama Eagle dengan ketentuan:
+ *    - Merupakan turunan dari class Animal
+ *    - Memiliki method:
+ *      - fly yang mengembalikan nilai string "${this.name} sedang terbang!"
+ *    - Ketika diinstansiasi, properti isMammal harus bernilai false
+ * 4. Buatlah instance dari class Rabbit bernama "myRabbit" dengan ketentuan:
+ *    - properti name bernilai: "Labi"
+ *    - properti age bernilai: 2
+ * 5. Buatlah instance dari class Eagle bernama "myEagle" dengan ketentuan:
+ *    - properti name bernilai: "Elo"
+ *    - properti age bernilai: 4
+ */
 
-    const diff_ms = today - birtday.getTime();
-    const diffDate = new Date(diff_ms);
 
-    return diffDate.getFullYear() - 1970;
+// TODO
+class Animal {
+    constructor(name, age, isMammal) {
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
+}
+
+class Rabbit extends Animal {
+  constructor() {
+    this.isMammal = true;
+  }
+  eat() {
+    return `${this.name} sedang makan!`;
+  }
+}
+
+class Eagle extends Animal {
+  constructor() {
+    this.isMammal = false;
+  }
+  fly() {
+    return `${this.name} sedang terbang!`;
+  }
+}
+
+const myRabbit = new Rabbit('Labi', 2);
+const myEagle = new Eagle('Elo', 4);
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+  Animal, Rabbit, Eagle, myRabbit, myEagle,
 };
-
-console.log(myAge('1998-09-23'));
