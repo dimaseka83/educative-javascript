a16cb8f26fe0622e9f9b4dc97ee02f9ed593f32d
dhimaseka <dimaseka83@gmail.com>
2022-01-26 09:11:42 +0700

    push

--- script.js
+++ script.js
@@ -37,26 +37,14 @@ class Animal {
     }
 }
 
-class Rabbit extends Animal {
-  constructor() {
-    this.isMammal = true;
-  }
-  eat() {
-    return `${this.name} sedang makan!`;
-  }
+class Rabbit extends Animal{
+    constructor(name){
+      this.name = name;
+
+    }
 }
 
-class Eagle extends Animal {
-  constructor() {
-    this.isMammal = false;
-  }
-  fly() {
-    return `${this.name} sedang terbang!`;
-  }
-}
 
-const myRabbit = new Rabbit('Labi', 2);
-const myEagle = new Eagle('Elo', 4);
 
 /**
  * Jangan hapus kode di bawah ini
