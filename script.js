1f3df410305af1a468ad96175165e41d866a5efe
dhimaseka <dimaseka83@gmail.com>
2022-01-27 10:54:56 +0700

    push

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-// String
-const months = ['March', 'Jan', 'Feb', 'Dec'];
-const sortM = months.sort();
-console.log(sortM);
+const scores = [70, 85, 90];
+const minimumScore = 65;
 
-// Number
-const array1 = [1,30,4,15,7,2,9,12,22,25,17,20,10];
-const compareNumber = (a,b) => {
-    return a - b;
-}
-
-const sorting = array1.sort(compareNumber);
-console.log(sorting);
+const examPassed = scores.every(score => score >= minimumScore);
+console.log(examPassed);
