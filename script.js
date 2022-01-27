9b8396a270405f74a9a2d96723883bfcec36ab97
dhimaseka <dimaseka83@gmail.com>
2022-01-27 12:08:34 +0700

    push

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
+/**
+ * TODO:
+ * Buatlah variabel greatAuthors yang merupakan array
+ * berdasarkan hasil filter() dan map() dari books:
+ *   - Gunakan fungsi filter untuk mengembalikan nilai item books
+ *     yang hanya memiliki nilai sales lebih dari 1000000.
+ *   - Gunakan fungsi map pada books yang sudah ter-filter,
+ *     untuk mengembalikan nilai string dengan format:
+ *     - `${author} adalah penulis buku ${title} yang sangat hebat!`
+ *
+ * Catatan: Jangan ubah nilai atau struktur dari books
+ */
 
-names.forEach((name) => {
-    console.log(`Hello, ${name}!`);
-});
-// Model Break atau Continue
-console.log("Batas");
-for (let i = 0; i < names.length; i++) {
-    if (names[i] === 'Jeff') continue;
-    console.log(`Hello, ${names[i]}!`);
-}
+ const books = [
+    { title: 'The Da Vinci Code', author: 'Dan Brown', sales: 5094805 },
+    { title: 'The Ghost', author: 'Robert Harris', sales: 807311 },
+    { title: 'White Teeth', author: 'Zadie Smith', sales: 815586 },
+    { title: 'Fifty Shades of Grey', author: 'E. L. James', sales: 3758936 },
+    { title: 'Jamie\'s Italy', author: 'Jamie Oliver', sales: 906968 },
+    { title: 'I Can Make You Thin', author: 'Paul McKenna', sales: 905086 },
+    { title: 'Harry Potter and the Deathly Hallows', author: 'J.K Rowling', sales: 4475152 },
+  ];
+  
+  // TODO
+  const result = books.filter((book) =>book.sales > 1000000);
+    const greatAuthors = result.map((book) => `${book.author} adalah penulis buku ${book.title} yang sangat hebat!`);
+
+  console.log(result);
+  /**
+   * Jangan hapus kode di bawah ini
+   */
+  
+//   module.exports = { books, greatAuthors };
+  
