fc7c3829c2cc8a18c6e446620f3aaa3413457d1e
dhimaseka <dimaseka83@gmail.com>
2022-01-28 13:53:12 +0700

    push

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
-const stock = {
-  coffeBeans: 250,
-  water: 1000
+const state = {
+  stock: {
+    coffeeBeans: 250,
+    water: 1000,
+  },
+  isCoffeeMachineBusy: true,
 }
 
-const checkStock = () => {
+const checkAvailability = () => {
   return new Promise((resolve, reject) => {
-    if (stock.coffeBeans >= 16 && stock.water >= 250) {
-        resolve("Stok cukup. Bisa membuat kopi");
-    }else {
-      reject("Stok tidak cukup");
-    }
+    setTimeout(() => {
+      if (!state.isCoffeeMachineBusy) {
+          resolve("Mesin kopi siap digunakan.");
+      }else{
+        reject("Maaf, mesin sedang sibuk.");
+      }
+    }, 1000);
   })
 }
 
-const handleSuccess = resolvedValue => {
-  console.log(resolvedValue);
+const checkStock = () => {
+    return new Promise((resolve, reject) => {
+      state.isCoffeeMachineBusy = true;
+      setTimeout(() => {
+        if (state.stock.coffeeBeans >= 16 && state.stock.water >= 250) {
+            resolve("Stok cukup. Bisa membuat kopi.");
+        }else{
+          reject("Stok tidak cukup!");
+        }
+      }, 1500);
+    })
 }
 
-const handleFailure = rejectionReason => {
-  console.log(rejectionReason);
+const brewCoffee = () => {
+  console.log("Membuatkan kopi Anda...");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Kopi sudah siap!")
+    }, 2000);
+  })
 }
 
-checkStock().then(handleSuccess, handleFailure);
+function makeEspresso() {
+    checkAvailability().then((value) => {
+      console.log(value);
+      return checkStock();
+    }).then((value) => {
+      console.log(value);
+      return brewCoffee();
+    }).then((value) => {
+      console.log(value);
+    }).catch((rejectedReason) => {
+      console.log(rejectedReason);
+    })
+}
+
+makeEspresso();
