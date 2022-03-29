99d3b88c34a489b195c099054bd83b2868883487
dhimaseka <dimaseka83@gmail.com>
2022-03-29 14:40:56 +0700

    ++

--- project_test/script.js
+++ project_test/script.js
@@ -1,15 +1,28 @@
-function menuItemEnter() {
+function showSubMenu() {
     const submenu = document.getElementsByClassName("menu__sub")[0];
     submenu.style.display = "block";
 }
 
-function menuItemLeave() {
+function hideSubMenu() {
   const submenu = document.getElementsByClassName("menu__sub")[0];
   submenu.style.display = "none";
 }
 
+let active = null;
+
+function onMenuItemMouseEnter(item){
+  if (active) {
+    item.classList.add("menu__main__item--active");
+  }
+  active = item;
+  item.classList.add("menu__main__item--active");
+  showSubMenu();
+}
+
 const menuItems = document.getElementsByClassName("menu__main__item");
 for (const menuItem of menuItems) {
-  menuItem.onmouseenter = menuItemEnter
-  menuItem.onmouseleave = menuItemLeave
+  menuItem.onmouseenter = showSubMenu(menuItem);
 }
+
+const menu = document.getElementsByClassName("menu")[0];
+menu.onmouseleave = hideSubMenu;
